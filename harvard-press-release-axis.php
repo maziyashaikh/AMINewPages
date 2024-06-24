f15a21c8297bcec7c1eaf5c360af5c25b83759fd
maziyashaikh <maziya.techinfinity@gmail.com>
2024-06-24 16:48:51 +0530

    Add files via upload

---
+++ harvard-press-release-axis.php
@@ -0,0 +1,344 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Axis My India - Connecting India to Build India</title>
+    <!-- CSS Links -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!--Favicon -->
+    <link rel="icon" href="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/05/favicon.png"
+        type="image/png">
+    <!-- Main File CSS Link -->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="responsive.css">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body style="background:white;">
+    <!-- Header Start -->
+    <nav>
+        <div class="navbar">
+          
+          <div class="logo"><a href="indexaxis.php"><img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/05/axislogo.png"></a></div>
+          
+          <i class='bx bx-menu'></i><div class="nav-links">
+            <div class="sidebar-logo">
+              <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/05/axislogo.png">
+              <i class='bx bx-x' ></i>
+            </div>
+            <ul class="links">
+              <li><a href="indexaxis.php">Home</a></li>
+          
+              <li>
+                <a href="#">About</a>
+                <i class='bx bxs-chevron-down js-arrowss arrowss '></i>
+                <ul class="js-sub-menu sub-menu">
+                  <li><a href="about-us-axis.php">Our Vision</a></li>
+                  <li><a href="harvard-press-release-axis.php">HBS Case Study</a></li>
+                  <li><a href="books.php">Book Launch</a></li>
+                  <li><a href="perform-rec.php">Track Record</a></li>
+                  <li><a href="privacy.php">Privacy Policy</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Solutions</a>
+                <i class='bx bxs-chevron-down htmlcss-arrow arrowss'></i>
+                <ul class="htmlCss-sub-menu sub-menu">
+                  <li><a href="never_beforeaxis.php">AMI Offering</a></li>
+                      <li><a href="AMI-PEP.php">'a' App</a></li>
+                  <li class="more">
+                    <span><a href="market-research-survey.php">Market Research & Survey</a>
+                    <i class='bx bxs-chevron-right arrowss more-arrow'></i>
+                  </span>
+                    <ul class="more-sub-menu sub-menu">
+                      <li><a href="trustindex/index.php">Consumer Trust Index</a></li> 
+                      <li><a href="Consumer-Sentiment.php">Consumer Sentiment Index</a></li>
+                      <li><a href="trustindex/consumercontentpreferencestudy.php">Consumer Content Preference Study</a></li>
+                      <li><a href="trustindex/allMediameasurementstudy.php">All Media Measurement Study</a></li>
+                      <li><a href="trustindex/finance.php">Financial Services</a></li>
+                              <li><a href="personalised-mass-media.php">Personalised Mass Media</a></li>
+                      <li><a href="productsampling.php">Product Sampling</a></li>
+                      <li><a href="printing-press.php">State of the Art Printing Press</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li><a href="clients.php">Clients</a></li>
+              <li><a href="press-releases.php">Media Center</a></li>
+              <li><a href="careersaxis.php">Careers</a></li>
+              <li><a href="contactusaxis.php">Contact</a></li>
+            </ul>
+          </div>
+          <div class="search-box">
+            <i class='bx bx-search'></i>
+            <div class="input-box">
+              <input type="text" placeholder="Search...">
+            </div>
+          </div>
+          <a class="nav-link trackbtn" href="perform-rec.php"><button class="btn btn-primary" style="font-size: 14px;">Election Tracker</button></a>
+        </div>
+      </nav>
+    <!-- Header End -->
+
+<div class="container mt-5">
+        <div class="row">
+            <div class="col-12 col-md-12 col-lg-7 content">
+                <h2 style="text-align: left; font-weight: 600;text-transform: capitalize;">Harvard Business School builds a case study on Axis My India</h2>
+                <p style="text-align: justify;">Axis My India, is the first consumer data intelligence firm from the country to have become a part of the Harvard Business School curriculum. The Ivy League business school has built a case study based on its election forecasting model.</p>
+                <p style="text-align: justify;">The case study, now a part of HBS’s classroom course on elections, highlights the complexity associated with successfully predicting elections in the world’s largest democracy, its diversity evident to all, with varied geographies, shared borders with six nations, sprawling rural populations and 23 different languages spoken across the country.</p>
+                <p style="text-align: justify;">This case study written by Professor Ananth Raman, Senior Associate Ann Winslow and Research Associate Kairavi Dey discusses the process which goes behind forecasting elections in India and the methods AMI uses to reach such accurate forecasts – selection of field surveyors, hiring & training, use of technology for data collection, quality auditing, data analysis & final forecasting.</p>
+                <p style="font-size: 21px; color: #9C27B0; font-weight: 600;">Click Here For:</p>
+                <div class="button-flex harvardbut">
+                <a class="nav-link trackbtn harbutton" href="https://axismyindia.org/HBS_Study.pdf"><button class="btn btn-primary" style="font-size: 17px;">Detailed Press Release</button></a>
+                
+                    <a class="nav-link trackbtn harbutton" href="https://www.hbs.edu/faculty/Pages/item.aspx?num=59270"><button class="btn btn-primary" style="font-size: 17px;">Harvard Business School Case Study</button></a>
+</div>
+            </div>
+            <div class="col-12 col-md-12 col-lg-5 image respimageharvad" style=" text-align: center;">
+                <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/Harvard-India-Post2.jpg" alt="Axis My India Image" style="max-width: 90%; width: 100%; border-radius: 43px; box-shadow: 0px 0px 7px #00000063; text-align: center;">
+            </div>
+        </div>
+    </div>
+    <div class="ag-format-container">
+       
+  <div class="ag-courses_box">
+    <!-- row one -->
+    <div class="row ag-row">
+    <div class="col-12 col-md-6 col-lg-3 ag-courses_item">
+      <div href="#" class="ag-courses-item_link" style="height: 227px;">
+        <div class="ag-courses-item_bg"></div>
+        <div class="ag-courses-image">
+            <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/ETTT.jpeg" alt="ET">
+        </div>
+        <div class="ag-courses-item_title">
+            <a href="https://economictimes.indiatimes.com/industry/services/education/harvard-business-school-builds-a-case-study-on-axis-my-india/articleshow/81276283.cms"> Business School builds a case study on Axis My India</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-12 col-md-6 col-lg-3 ag-courses_item">
+      <div href="#" class="ag-courses-item_link" style="height: 229px;">
+        <div class="ag-courses-item_bg"></div>
+        <div class="ag-courses-image">
+            <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/mintlogo.png" alt="Mint">
+        </div>
+        <div class="ag-courses-item_title">
+            <a href="https://www.livemint.com/news/india/exit-poll-firm-axis-my-india-is-now-a-case-study-at-harvard-business-school-11614585910666.html"> Poll firm Axis My India is now a case study at Harvard Business School</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-12 col-md-6 col-lg-3 ag-courses_item">
+      <div href="#" class="ag-courses-item_link">
+        <div class="ag-courses-item_bg"></div>
+        <div class="ag-courses-image">
+            <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/Aaj-tak.png" alt="Aaj Tak">
+        </div>
+        <div class="ag-courses-item_title">
+           <a href="https://www.aajtak.in/india/news/story/harvard-business-school-case-study-on-axis-my-india-predicts-elections-india-today-1219871-2021-03-10"> एक्सिस माई इंडिया देश की ऐसी पहली कंज्यूमर डेटा इंटेलीजेंस फर्म हो गई है जिसे हार्वर्ड बिजनेस स्कूल के पाठ्यक्रम में जगह मिली है |</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-12 col-md-6 col-lg-3 ag-courses_item">
+      <div href="#" class="ag-courses-item_link" style="height: 229px;">
+        <div class="ag-courses-item_bg"></div>
+        <div class="ag-courses-image">
+            <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/ET.jpg" alt="India Today">
+        </div>
+        <div class="ag-courses-item_title">
+           <a href="https://www.indiatoday.in/india/story/india-today-election-survey-partner-axis-my-india-harvard-business-school-curriculum-1777615-2021-03-10"> India Today’s election survey partner, Axis My India makes it to Harvard Business School curriculum</a>
+        </div>
+      </div>
+    </div>
+</div>
+<!-- row two -->
+<div class="row ag-row">
+  <div class="col-12 col-md-6 col-lg-3 ag-courses_item">
+    <div href="#" class="ag-courses-item_link" style="height: 227px;">
+      <div class="ag-courses-item_bg"></div>
+      <div class="ag-courses-image">
+          <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/Outlook.png" alt="Outlook">
+      </div>
+      <div class="ag-courses-item_title">
+        <a href="https://www.outlookindia.com/"> Business School builds case study on Axis My India’s poll forecasting model</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-12 col-md-6 col-lg-3 ag-courses_item">
+    <div href="#" class="ag-courses-item_link" style="height: 229px;">
+      <div class="ag-courses-item_bg"></div>
+      <div class="ag-courses-image">
+          <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/yahoo-news.png" alt="Yahoo News" style="height: 42px;">
+      </div>
+      <div class="ag-courses-item_title">
+       <a href="https://in.search.yahoo.com/?fr2=inr"> Harvard Business School builds case study on Axis My India’s poll forecasting model</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-12 col-md-6 col-lg-3 ag-courses_item">
+    <div href="#" class="ag-courses-item_link">
+      <div class="ag-courses-item_bg"></div>
+      <div class="ag-courses-image">
+          <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/JD2.png" alt="JustDial Social">
+      </div>
+      <div class="ag-courses-item_title">
+        <a href="https://www.justdial.com/JdSocial/news/1614609282746000">Harvard Business School builds case study on Axis My India’s poll forecasting model</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-12 col-md-6 col-lg-3 ag-courses_item">
+    <div href="#" class="ag-courses-item_link" style="height: 229px;">
+      <div class="ag-courses-item_bg"></div>
+      <div class="ag-courses-image">
+          <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/HigEdu2.png" alt="India Today">
+      </div>
+      <div class="ag-courses-item_title">
+        <a href="https://www.highereducationdigest.com/harvard-business-school-builds-a-case-study-on-axis-my-india/"> Business School builds a case study on Axis My India  </a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- row three -->
+<div class="row ag-row">
+  <div class="col-12 col-md-6 col-lg-3 ag-courses_item">
+    <div href="#" class="ag-courses-item_link" style="height: 227px;">
+      <div class="ag-courses-item_bg"></div>
+      <div class="ag-courses-image">
+          <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/indiaeducationdiary.png" alt="India">
+      </div>
+      <div class="ag-courses-item_title">
+        <a href="https://indiaeducationdiary.in/harvard-business-school-builds-a-case-study-on-axis-my-india/">Harvard Business School builds a case study on Axis My India</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-12 col-md-6 col-lg-3 ag-courses_item">
+    <div href="#" class="ag-courses-item_link" style="height: 229px;">
+      <div class="ag-courses-item_bg"></div>
+      <div class="ag-courses-image">
+          <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/devdiscourselogo.webp" alt="Devdiscourse">
+      </div>
+      <div class="ag-courses-item_title">
+        <a href="https://www.devdiscourse.com/article/business/1477782-harvard-business-school-builds-case-study-on-axis-my-indias-poll-forecasting-model"> Business School builds case study on Axis My India’s poll forecasting model</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-12 col-md-6 col-lg-3 ag-courses_item">
+    <div href="#" class="ag-courses-item_link">
+      <div class="ag-courses-item_bg"></div>
+      <div class="ag-courses-image">
+          <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/BollyInside.png" alt="BollyInside">
+      </div>
+      <div class="ag-courses-item_title">
+      <a href="https://www.bollyinside.com/news/india-news/harvard-business-school-builds-case-study-on-axis-my-indias-poll-forecasting-model/">  Harvard Business School builds case study on Axis My India’s poll forecasting model</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-12 col-md-6 col-lg-3 ag-courses_item">
+    <div href="#" class="ag-courses-item_link" style="height: 229px;">
+      <div class="ag-courses-item_bg"></div>
+      <div class="ag-courses-image">
+          <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/Crux2.png" alt="Cruxs Digital">
+      </div>
+      <div class="ag-courses-item_title">
+       <a href="https://cruxsdigital.com/%E0%A4%B9%E0%A4%BE%E0%A4%B0%E0%A5%8D%E0%A4%B5%E0%A4%B0%E0%A5%8D%E0%A4%A1-%E0%A4%AC%E0%A4%BF%E0%A4%9C%E0%A4%A8%E0%A5%87%E0%A4%B8-%E0%A4%B8%E0%A5%8D%E0%A4%95%E0%A5%82%E0%A4%B2-%E0%A4%8F%E0%A4%95/"> हार्वर्ड बिजनेस स्कूल एक्सिस माई इंडिया पर एक केस स्टडी बनाता है </a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- row four -->
+<div class="row ag-row">
+  <div class="col-12 col-md-6 col-lg-3 ag-courses_item">
+    <div href="#" class="ag-courses-item_link" style="height: 227px;">
+      <div class="ag-courses-item_bg"></div>
+      <div class="ag-courses-image">
+          <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/NFA2.png" alt="NFA Post">
+      </div>
+      <div class="ag-courses-item_title">
+        <a href="https://www.thenfapost.com/2021/03/02/harvard-business-school-builds-case-study-on-axis-my-indias-election-forecasting-model/"> Business School builds case study on Axis My India’s election forecasting model</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-12 col-md-6 col-lg-3 ag-courses_item">
+    <div href="#" class="ag-courses-item_link" style="height: 229px;">
+      <div class="ag-courses-item_bg"></div>
+      <div class="ag-courses-image">
+          <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/apn-news.png" alt="APN News">
+      </div>
+      <div class="ag-courses-item_title">
+        <a href="#">Harvard Business School builds a case study on Axis My India</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-12 col-md-6 col-lg-3 ag-courses_item">
+    <div href="#" class="ag-courses-item_link">
+      <div class="ag-courses-item_bg"></div>
+      <div class="ag-courses-image">
+          <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/PTI.webp" alt="PTI">
+      </div>
+      <div class="ag-courses-item_title">
+        <a href="#">Harvard Business School builds case study on Axis My India’s poll forecasting model</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-12 col-md-6 col-lg-3 ag-courses_item">
+    <div href="#" class="ag-courses-item_link" style="height: 229px;">
+      <div class="ag-courses-item_bg"></div>
+      <div class="ag-courses-image">
+          <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/FinEx.png" alt="Financial Express">
+      </div>
+      <div class="ag-courses-item_title">
+        <a href="#">Harvard Business School builds a case study on Axis My India</a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- row five -->
+<div class="row ag-row hbr">
+  <div class="col-12 col-md-6 col-lg-6 ag-courses_item">
+    <div href="#" class="ag-courses-item_link" style="height: 227px;">
+      <div class="ag-courses-item_bg"></div>
+      <div class="ag-courses-image">
+          <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/biztak.jpg" alt="BizTak">
+      </div>
+      <div class="ag-courses-item_title">
+        <a href="#"> Axis My India का विदेशों में बजा डंका, हार्वर्ड बिजनेस स्कूल में की जाएगी कंपनी पर केस स्टडी</a>
+      </div>
+      <div class="ag-video">
+        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/3I9RR3ZnEMY?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-12 col-md-6 col-lg-6 ag-courses_item">
+    <div href="#" class="ag-courses-item_link" style="height: 229px;">
+      <div class="ag-courses-item_bg"></div>
+      <div class="ag-courses-image">
+          <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/lallantop.webp" alt="The Lallantop">
+      </div>
+      <div class="ag-courses-item_title">
+        <a href="#"> Lallantop, country’s ‘New Age’ Hindi news website, interview with Pradeep Gupta</a>
+      </div>
+      <div class="ag-video">
+        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/CP9_LwWyBC8?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+      </div>
+    </div>
+  </div>
+</div>
+  </div>
+</div>
+<?php include("footeraxis.php")?>
