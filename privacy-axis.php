9a53fde99eb176b00ca51461c849c57694d7e05c
maziyashaikh <maziya.techinfinity@gmail.com>
2024-06-26 11:44:21 +0530

    Add files via upload

---
+++ privacy-axis.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Axis My India - Connecting India to Build India</title>
+    <!-- CSS Links -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!--Favicon -->
+    <link rel="icon" href="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/05/favicon.png"
+        type="image/png">
+    <!-- Main File CSS Link -->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="responsive.css">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body style="background:white;">
+    <!-- Header Start -->
+    <nav>
+        <div class="navbar">
+          
+          <div class="logo"><a href="indexaxis.php"><img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/05/axislogo.png"></a></div>
+          
+          <i class='bx bx-menu'></i><div class="nav-links">
+            <div class="sidebar-logo">
+              <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/05/axislogo.png">
+              <i class='bx bx-x' ></i>
+            </div>
+            <ul class="links">
+              <li><a href="indexaxis.php">Home</a></li>
+          
+              <li>
+                <a href="#">About</a>
+                <i class='bx bxs-chevron-down js-arrowss arrowss '></i>
+                <ul class="js-sub-menu sub-menu">
+                  <li><a href="about-us-axis.php">Our Vision</a></li>
+                  <li><a href="harvard-press-release.php">HBS Case Study</a></li>
+                  <li><a href="books.php">Book Launch</a></li>
+                  <li><a href="perform-rec.php">Track Record</a></li>
+                  <li><a href="privacy.php">Privacy Policy</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Solutions</a>
+                <i class='bx bxs-chevron-down htmlcss-arrow arrowss'></i>
+                <ul class="htmlCss-sub-menu sub-menu">
+                  <li><a href="never_beforeaxis.php">AMI Offering</a></li>
+                      <li><a href="AMI-PEP.php">'a' App</a></li>
+                  <li class="more">
+                    <span><a href="market-research-survey.php">Market Research & Survey</a>
+                    <i class='bx bxs-chevron-right arrowss more-arrow'></i>
+                  </span>
+                    <ul class="more-sub-menu sub-menu">
+                      <li><a href="trustindex/index.php">Consumer Trust Index</a></li> 
+                      <li><a href="Consumer-Sentiment.php">Consumer Sentiment Index</a></li>
+                      <li><a href="trustindex/consumercontentpreferencestudy.php">Consumer Content Preference Study</a></li>
+                      <li><a href="trustindex/allMediameasurementstudy.php">All Media Measurement Study</a></li>
+                      <li><a href="trustindex/finance.php">Financial Services</a></li>
+                              <li><a href="personalised-mass-media.php">Personalised Mass Media</a></li>
+                      <li><a href="productsampling.php">Product Sampling</a></li>
+                      <li><a href="printing-press.php">State of the Art Printing Press</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li><a href="clients.php">Clients</a></li>
+              <li><a href="press-releases.php">Media Center</a></li>
+              <li><a href="careersaxis.php">Careers</a></li>
+              <li><a href="contactusaxis.php">Contact</a></li>
+            </ul>
+          </div>
+          <div class="search-box">
+            <i class='bx bx-search'></i>
+            <div class="input-box">
+              <input type="text" placeholder="Search...">
+            </div>
+          </div>
+          <a class="nav-link trackbtn" href="perform-rec.php"><button class="btn btn-primary" style="font-size: 14px;">Election Tracker</button></a>
+        </div>
+      </nav>
+    <!-- Header End -->
+ 
+ <!-- Main Slider Start -->
+    <div class="hero pripol">
+        <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/privacy.png" alt="Background Image" class="background-image">
+        <div class="hero-content">
+            <span>Axis My India</span>
+            <h1>Privacy Policy</h1>
+        </div>
+    </div>
+    <!-- Main Slider End -->
+
+  <!-- Home Main Section Start -->
+  <div class="container privacyp" style="padding-top: 90px; padding-bottom: 90px;">
+    <div class="col-lg-12">
+         <p>The details of the viewer submitted in the form will not be disclosed or sold to anybody.<br>
+    The details will remain anonymous with AXIS MY INDIA. Details are required only for our records to maintain.</p>
+    <p>Axis My India Ltd ("Axis My India(Insight/Surveys)("Insights/Surveys"). Such information from Surveys as reflects the past and present will enable Clients with Axis My India (Insight/Surveys) seeks to gather various opinions and feelings of different persons with different standpoints. Variety of opinions and feelings is the key value we deliver to society. Throughout the operation of our business, Axis My India (Insight/Surveys) as well as visitors of Axis My India (Insight/Surveys) will comply with any applicable laws concerning the handling of personal data and also the ICC/ESOMAR Code, which is the code of conduct for the marketing research industry.(any code to verify or to remove line) Personal data ("Personal Data") means any information relating to an identified or identifiable natural person. Some categories of Personal Data will reveal the User(Insight/Surveys) will also adhere to applicable laws when collecting or processing any other data that the applicable law categorizes as "sensitive" or "special categories". Axis My India Ltd will never ask personal Bank Details from any Member/respondent. Axis My India Ltd do not have any intermediaries or agents or franchisees for enrollment of Membership. Axis My India Ltd will not be responsible for any payment made by individuals for getting enrolled as a member. Your membership will be valid only if your Demographic, Geographic and Socio-economic details are shared since unless your Gender, Age, Education, Occupation, Income and physical postal address are provided the entities who wish to connect with you may it be the Government, NGOs, Corporates will not be able to consider you for their proposed activity. Hence the Primary data detailed above is compulsory and not optional. Please treat this as a mandatory compliance for getting enrolled as a Member By creating an account, the User hereby acknowledges and agrees to the processing of the UserInsight / Surveys) Collects The types of Personal Data Axis My India ( s contact details (such as e-mail address,name,date of birth,gender,physical address,phone number,and if necessary payment information) and basic attribute information suggesting your lifestyle or relating to your life stage (such as relates to your family, education, income, employment, and, only when absolutely necessary, race);</p>
+    <p style="color: #933993; font-weight: 600; text-shadow: 1px 1px #0000002e;">"Detailed Profile", which contains more detailed information about your lifestyle and life stage than Basic Profile;</p>
+    <p style="color: #933993; font-weight: 600; text-shadow: 1px 1px #0000002e;">"Survey Data" of the answers you made in a Survey;</p>
+    <p>Records and history of your usage of Axis My India(Insight/Surveys); and
+        Cookie data (see the following section for details)and server logs which Axis My India(Insight/Surveys) may collect directly from a User Sensitive Personal Data, especially racial information in Basic Profile in some countries Detailed Profile and Surveys, where permitted under applicable local laws and if the collection is necessary. In that case, Axis My India ( Insight / Surveys) does not collect Personal Data of persons less than 16 years of age, as Axis My India ( s services are not directed at persons less than 16 years of age.(IT WAS 10 YEARS??)</p>
+        <p style="font-weight: 600; font-size: 20px; color: #933993;">How Axis My India (Insight/Surveys) will use Personal Data for the following purposes:</p>
+        <ul>
+            <li>Create and maintain your membership, using your Basic Profile;</li>
+            <li>Screen and send appropriate surveys to candidates, using your Basic Profile</li>
+            <li>Further screen and choose such candidates, using your Detailed Profile</li>
+            <li>Conduct a survey, which may be initiated by Axis My India (Insight/Surveys) offers,using your Basic Profile and Records.</li>
+            <li>Respond to your inquiries, when Axis My India (Insight/Surveys),using various Personal Data we obtain.</li>
+            <li>Prevent fraud, malicious activities, and any losses associated with such, using various Personal Data that we already have.</li>
+            <li>The Personal Data contained in a Survey, including Basic Profile and Detailed Profile, will be processed and used after aggregation or pseudonimization by deleting at least such direct identifiers as names, addresses, e-mail addresses, etc.,so that Axis My India (Insight/Surveys) and/or Client may collect and use in that specific Survey with appropriate communication with you including notification to draw the Users legitimate interests; and</li>
+            <li>Your consent in some cases especially for the paragraph 1 (3) of this section.</li>
+            <li>If we ask for your consent to process your Personal Data, we will ensure that you receive all the necessary information about the processing and the relevant purpose before your consent and that you may withdraw your consent at any time, but not later to the processing of such Data by us after seeking your consent and any withdrawal of such consent after the said event shall not be construed as a breach of this agreement by Axis My India (Insight/Surveys) and our Clients are different and independent businesses, Axis My India's ( misuses,leakages and other data breaches beyond reasonable controls of Axis My India (Insight/Surveys) strongly recommends that Users carefully read through privacy statements and policies of the Client and acknowledge the controller(s),the purposes of use of your Personal Data and your rights before answering a Survey.
+            </li>
+            <li>Your provision of Personal Data to Axis My India (Insight/Surveys) with certain Personal Data necessary for our performance of the contract and our legitimate interests, you will not be able to gain access to our services and/or enjoy the benefits of having your membership.</li>
+            <li>Your Basic Profile and Detailed Profile can be edited, corrected, and for Detailed Profile, deleted at any time when you have access to your account on My Page. Axis My India (Insight/Surveys).</li>
+            <li>If requested in the way described in "Inquiries/Complaints", Axis My India (Insight/Surveys) will respond to your requests such as the following:
+                <ul class="pp">
+                    <li>Axis My India (Insight/Surveys) will cease using the account of any User who has been inactive for 1 year until the account is reactivated by logging into the service again.</li>
+                    <li>Where necessary to carry out our business operations,Axis My India (Insight/Surveys) may disclose your Personal Data to a third party, including Axis My India's (parent company,subsidiaries,and affiliates to the necessary extent.</li>
+                    <li>Axis My India (survey system for Users to answer a survey on it. Axis My India (Insight/Surveys) encourages the User to be aware when the User is leaving Axis My India's (Website,and to read the privacy statements of each website that collects Personal Data.This Policy applies solely to the processing activities collected and controlled by Axis My India ( Insight / Surveys) and its data protection officers will respond, without undue delay, to you or your representative.</li>
+                </ul>
+            </li>
+        </ul>
+        <p>Axis My India and its data protection officers
+            Please note that Axis My India(Insight/Surveys) will, in an appropriate way,communicate with you when any material changes occur within this Policy.</p>
+            <p><b style="color: #933993; font-weight: 600; text-shadow: 1px 1px #0000002e;">E-Reward/Benefits Policy:</b><br>
+                Once registered to Axis My India(Insight/Surveys) strictly protects and controls your privacy and personal information. To learn more about our privacy policy at Axis My India(INSIGHT/SURVEYS) SURVEYS, a market research panel owned by Axis My India ("Axis My India(INSIGHT/SURVEYS) SURVEYS,you voluntarily and unequivocally agree to and accept all of the terms and conditions set forth below. For more information on how we use the data that you provide and our privacy practices, please visit our privacy policy General</p>
+                <p>The objective of these rules("Member Agreement") is to stipulate the rights and obligations in the relationship between Axis My India(Insight/Surveys)") and the member who has been authorized by Axis My India (Insight/Surveys).</p>
+                <p>The Business is conducted by Axis My India (Insight/Surveys) or its Client (collectively the "Axis My India (Insight/Surveys) from any claims/damages or suits arising as a result thereof.</p>
+</div>
+  </div>
+<!-- Home Main Section Start -->
+
+
+  
+      <?php include("footeraxis.php")?>
