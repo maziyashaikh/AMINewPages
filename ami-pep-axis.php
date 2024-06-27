833a4659d1ecec6265283e9888d2c964f9d3230b
maziyashaikh <maziya.techinfinity@gmail.com>
2024-06-27 17:05:35 +0530

    Add files via upload

---
+++ ami-pep-axis.php
@@ -0,0 +1,126 @@
+<?php include('headeraxis1.php')?>
+
+<!-- Image section -->
+<div class="container pepsec">
+<div class="pepimg">
+    <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/People-Empowerment-Platform-Web-29-8-2023-scaled.jpg" alt="People Empowerment">
+</div>
+<h2 class="stylepep">Axis My India Partners with Google to Build Single-stop
+People Empowerment Platform Application</h2>
+
+<p style="font-weight: 600; color: #933993; text-align: center; margin-top: 30px;">
+Shri Ashwini Vaishnaw, Hon'ble Minister of Electronics and Information Technology, Communications and Railways, recently announced the 'a' app, a People Empowerment platform, built by Axis My India in partnership with Google. The ‘a’ app is being built with the Purpose of empowering citizens with authentic information and solutions based on their needs and aspirations.</p>
+
+<p style="text-align: left;">Read more on this : <b><a href="https://goo.gle/3Q6yFeA" style="color: #933993; text-decoration: underline;">https://goo.gle/3Q6yFeA</a></b></p>
+
+<p>India’s leading consumer data intelligence company, <span style="color: #933993;">Axis My India</span>, today announced that it is building a one-of-a-kind platform leveraging Google technology that aims to bring about a step change in people’s awareness, accessibility, and utilization of a slew of services. These include Government social welfare schemes, basic day-to-day amenities, employment opportunities, and healthcare benefits, and has the potential to benefit up to 250 million Indian households across the country.</p>
+<p>Built on <span style="color: #933993;">Google Cloud’s</span> scalable and secure infrastructure, Axis My India’s <span style="color: #933993;">People Empowerment Platform (PEP)</span> application will use Google Cloud’s generative AI (GenAI) technology to provide relevant information about government schemes and desired communication/content to citizens based on their individual and family needs. In order to be useful to people across geographies, the PEP will be designed to support 13 regional languages employing Google translation API and Vertex AI for real-time translation and transliteration of information. This two-way communication will facilitate enhanced engagement with Axis My India’s Mission <span style="color: #933993;">‘To Actively Contribute Every day to Transform a Billion Lives’</span> by acting as a digital and physical ‘Jan Suvidha Kendra’ (Public Welfare Centre). The roll-out of PEP will be supported by Axis My India’s network of local offices in more than <span style="color: #933993;">700</span> districts across India.</p>
+<p>Speaking on the partnership, <span style="color: #933993;">Pradeep Gupta, Chairman and Managing Director, Axis My India, said,</span> “Our collaboration with Google marks a pivotal moment in our pursuit to positively impact the lives of a billion Indians. By leveraging Google Cloud technologies, we aim to create an inclusive platform that truly empowers citizens with authentic information and solutions based on their needs. Axis My India’s massive scale and expertise affords us an unprecedented opportunity to bring information equity between rural and urban India through our platform. We are certain that we will achieve our ambitious goal of acting as a digital super-app for all 250 million Indian households.”</p>
+<p>“Google Cloud offers a wide range of services that can benefit business in terms of scalability, flexibility, security, performance, innovation and support. Rising mobile connectivity presents the opportunity to create an inclusive, equitable, and safer internet for everyone, no matter their literacy, language, or location. We are excited to work with Axis My India to help advance their vision with the People Empowerment Platform, backed by state-of-the-art Gen AI Cloud technologies from Google Cloud. This will help them bring government services and programs to more Indians across the country," said <span style="color: #933993;">Bikram Singh Bedi, Managing Director, Google Cloud India.</span></p>
+<p>Axis My India announced their vision for PEP in 2021 in line with its Mission Himalaya – Unchi Udaan Roadmap Initiative and is currently in the advanced stages of developing this with Google, with the pilot version of the PEP app expected to launch in October 2023.</p>
+
+<!-- Card Section -->
+<div class="flipwrapper">
+    <div class="flipcols">
+        <!-- row one -->
+         <div class="col-lg-4">
+              <div class="flipcol" ontouchstart="this.classList.toggle('hover');">
+                  <div class="flipcontainer">
+                    <a href="https://www.business-standard.com/companies/news/google-cloud-inks-pact-with-axis-my-india-to-build-a-super-app-123101901038_1.html">
+                      <div class="flipfront" style="background-image: url(https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/business-standard.png)">
+                      </div>
+                      <div class="flipback">
+                          <div class="flipinner">
+                            <p>Access to authentic information to meet everyday needs</p>
+                          </div>
+                      </div>
+                      </a>
+                  </div>
+              </div>
+              </div>
+              <div class="col-lg-4">
+              <div class="flipcol" ontouchstart="this.classList.toggle('hover');">
+                  <div class="flipcontainer">
+                    <a href="https://www.businesstoday.in/technology/news/story/google-collaborates-with-axis-my-india-to-create-multilingual-super-app-a-403045-2023-10-23">
+                      <div class="flipfront" style="background-image: url(https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/business-today.png)">
+                      </div>
+                      <div class="flipback">
+                          <div class="flipinner">
+                              <p>Axis My India, in partnership with Google Cloud is building the 'a' app</p>
+                          </div>
+                      </div>
+                      </a>
+                  </div>
+              </div>
+              </div>
+              <div class="col-lg-4">
+              <div class="flipcol" ontouchstart="this.classList.toggle('hover');">
+                  <div class="flipcontainer">
+                    <a href="https://blog.google/intl/en-in/partnering-indias-success-in-a-new-digital-paradigm">
+                      <div class="flipfront" style="background-image: url(https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/India-Express.png)">
+                      </div>
+                      <div class="flipback">
+                          <div class="flipinner">
+                              <p>Google Cloud to power super app for citizen’s services.</p>
+                          </div>
+                      </div>
+                      </a>
+                  </div>
+              </div>
+              </div>
+              </div>
+              <div class="flipcols" style="margin-bottom: 70px;">
+                <!-- row two -->
+                <div class="col-lg-4">
+              <div class="flipcol" ontouchstart="this.classList.toggle('hover');">
+                  <div class="flipcontainer">
+                    <a href="https://bfsi.economictimes.indiatimes.com/news/industry/google-cloud-partners-with-axis-my-india-to-build-a-multilingual-super-app/104555021">
+                      <div class="flipfront" style="background-image: url(https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/ET.png)">
+                      </div>
+                      <div class="flipback">
+                          <div class="flipinner">
+                              <p>Google Cloud partners with Axis My India to build "a" multilingual Super-app</p>
+                          </div>
+                      </div>
+                      </a>
+                  </div>
+              </div>
+              </div>
+              <div class="col-lg-4">
+              <div class="flipcol" ontouchstart="this.classList.toggle('hover');">
+                  <div class="flipcontainer">
+                    <a href="https://indiafrontline.com/google-collaborates-axis-my-india/">
+                      <div class="flipfront" style="background-image: url(https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/IndiaFrontline.png)">
+                      </div>
+                      <div class="flipback">
+                          <div class="flipinner">
+                              <p> Google collaborates with Axis My India to create citizen centric super-app ‘a’</p>
+                          </div>
+                      </div>
+                      </a>
+                  </div>
+              </div>
+              </div>
+              <div class="col-lg-4">
+              <div class="flipcol" ontouchstart="this.classList.toggle('hover');">
+                  <div class="flipcontainer">
+                    <a href="https://indianexpress.com/article/technology/tech-news-technology/google-cloud-to-help-farmers-reap-benefits-of-ondc-8990417/">
+                      <div class="flipfront" style="background-image: url(https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/ALogo.png)">
+
+                      </div>
+                      <div class="flipback">
+                          <div class="flipinner">
+                              <p>Google Cloud to power super app for citizen’s services</p>
+                          </div>
+                      </div>
+                      </a>
+                  </div>
+              </div>
+              </div>
+              </div>
+          </div>
+   </div>
+</div>
+
+
+<?php include('footeraxis.php')?>
