4c6364762da30df43510c8863b426c56ad1d2821
maziyashaikh <maziya.techinfinity@gmail.com>
2024-06-28 18:52:00 +0530

    Add files via upload

--- trust-index.php
+++ trust-index.php
@@ -1,89 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Axis My India - Connecting India to Build India</title>
-    <!-- CSS Links -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <!--Favicon -->
-    <link rel="icon" href="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/05/favicon.png"
-        type="image/png">
-    <!-- Main File CSS Link -->
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="responsive.css">
-    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-
-<body class="offering" style="background: #fff;">
-    <!-- Header Start -->
-    <nav>
-        <div class="navbar">
-          
-          <div class="logo"><a href="indexaxis.php"><img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/05/axislogo.png"></a></div>
-          
-          <i class='bx bx-menu'></i><div class="nav-links">
-            <div class="sidebar-logo">
-              <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/05/axislogo.png">
-              <i class='bx bx-x' ></i>
-            </div>
-            <ul class="links">
-              <li><a href="indexaxis.php">Home</a></li>
-          
-              <li>
-                <a href="#">About</a>
-                <i class='bx bxs-chevron-down js-arrowss arrowss '></i>
-                <ul class="js-sub-menu sub-menu">
-                  <li><a href="about-us-axis.php">Our Vision</a></li>
-                  <li><a href="harvard-press-release.php">HBS Case Study</a></li>
-                  <li><a href="books.php">Book Launch</a></li>
-                  <li><a href="perform-rec.php">Track Record</a></li>
-                  <li><a href="privacy.php">Privacy Policy</a></li>
-                </ul>
-              </li>
-              <li>
-                <a href="#">Solutions</a>
-                <i class='bx bxs-chevron-down htmlcss-arrow arrowss'></i>
-                <ul class="htmlCss-sub-menu sub-menu">
-                  <li><a href="never_beforeaxis.php">AMI Offering</a></li>
-                      <li><a href="AMI-PEP.php">'a' App</a></li>
-                  <li class="more">
-                    <span><a href="market-research-survey.php">Market Research & Survey</a>
-                    <i class='bx bxs-chevron-right arrowss more-arrow'></i>
-                  </span>
-                    <ul class="more-sub-menu sub-menu">
-                      <li><a href="trustindex/index.php">Consumer Trust Index</a></li> 
-                      <li><a href="Consumer-Sentiment.php">Consumer Sentiment Index</a></li>
-                      <li><a href="trustindex/consumercontentpreferencestudy.php">Consumer Content Preference Study</a></li>
-                      <li><a href="trustindex/allMediameasurementstudy.php">All Media Measurement Study</a></li>
-                      <li><a href="trustindex/finance.php">Financial Services</a></li>
-                              <li><a href="personalised-mass-media.php">Personalised Mass Media</a></li>
-                      <li><a href="productsampling.php">Product Sampling</a></li>
-                      <li><a href="printing-press.php">State of the Art Printing Press</a></li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-              <li><a href="clients.php">Clients</a></li>
-              <li><a href="press-releases.php">Media Center</a></li>
-              <li><a href="careersaxis.php">Careers</a></li>
-              <li><a href="contactusaxis.php">Contact</a></li>
-            </ul>
-          </div>
-          <div class="search-box">
-            <i class='bx bx-search'></i>
-            <div class="input-box">
-              <input type="text" placeholder="Search...">
-            </div>
-          </div>
-          <a class="nav-link trackbtn" href="perform-rec.php"><button class="btn btn-primary" style="font-size: 14px;">Election Tracker</button></a>
-        </div>
-      </nav>
-    <!-- Header End -->
+<?php include('headeraxis1.php')?>
 
 <!-- Main Page Start -->
       <div class="offeringrect trustin">
@@ -280,11 +195,14 @@
         A wider range of information: <span style="color: #933993;">What is my brand preference by age, gender & geography? Am I losing share to Brand B especially in the 26-35 years age category? Am I going to lose my MS in State A and District C in the coming months? Do my consumers still read newspapers or are they spending their maximum time on Facebook?</span></p>
   </div>
   <div class="offeringrect" style="margin-top: 35px;">
-    <section>
+    <section class="deskoffer">
         <img src="https://shravani.haribhaumusicals.com/wp-content/uploads/2024/06/Group-123.png" class="rectoffer">
         <div class="offercenter trustsh">An Example Of Sample Outputs Related To Brand Retention & Shift Analysis:
         </div>
     </section>
+    <section class="moboffer">
+      <h3 style="font-weight: 600;color: #970198;">An Example Of Sample Outputs Related To Brand Retention & Shift Analysis:</h3>
+  </section>
     </div>
   <div class="container" style="padding: 20px;">
       <p style="text-align: center; font-size: 20px;color: #970198; font-weight: 600; line-height: 26px;">Current usage share for Brand A is <span class="bigtext">25%</span>, while it is likely to be <span class="bigtext">28%</span> in the coming times.
